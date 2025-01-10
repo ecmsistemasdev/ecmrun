b82ee735c18e216ef5680b41d679a922f36ce201
ecmsistemasdev <164636970+ecmsistemasdev@users.noreply.github.com>
2025-01-10 00:26:54 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, render_template, redirect, request, jsonify, flash
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port, debug=True)
+
