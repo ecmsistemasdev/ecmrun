21ad3886ea8990e810d26701c29600389b420d0b
ecmsistemasdev <164636970+ecmsistemasdev@users.noreply.github.com>
2025-05-29 09:31:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -217,6 +217,11 @@ def format_time_difference(seconds):
     seconds = int(seconds % 60)
     return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
+
+@app.route('/desafio200k/regulamento')
+def regulamento200k():
+    return render_template('regulamento200k.html')
+
 # Rotas do backyard
 @app.route('/backyard/lancamento')
 def backyard_lancamento():
