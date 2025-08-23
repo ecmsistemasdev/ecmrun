a6a12cf5527e06a21a6d676f465808705702894d
ecmsistemasdev <164636970+ecmsistemasdev@users.noreply.github.com>
2025-08-23 12:07:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -717,7 +717,7 @@ def comprovante(payment_id):
             send_email(receipt_data_dict)
         
             # Enviar notificação para o organizador
-            # send_organizer_notification(receipt_data_dict)
+            send_organizer_notification(receipt_data_dict)
 
             cur1 = mysql.connection.cursor()
             cur1.execute('''
@@ -8017,6 +8017,7 @@ def excluir_imagem(imagem_id):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
